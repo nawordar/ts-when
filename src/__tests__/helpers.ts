5fa4f95b795db0a41364f1a6694ed680a44565d0
nawordar <czarek@drozak.net>
2019-09-06 20:30:29 +0200

    Disable tslint at some lines

--- src/__tests__/helpers.ts
+++ src/__tests__/helpers.ts
@@ -2,4 +2,5 @@ export type IsSubtype<T, S> = S extends T ? true : false;
 
 export type IsType<T, S> = S extends T ? (T extends S ? true : false) : false;
 
+// tslint:disable-next-line: no-empty
 export const StaticCheck = <_T extends true>() => { };
