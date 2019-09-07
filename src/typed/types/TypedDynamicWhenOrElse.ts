9c24281de8415b0c7cff254b8a9c73042996312c
nawordar <czarek@drozak.net>
2019-09-07 20:35:14 +0200

    Simplify interface

--- src/typed/types/TypedDynamicWhenOrElse.ts
+++ src/typed/types/TypedDynamicWhenOrElse.ts
@@ -1,22 +1,5 @@
-import { Matcher } from "../../types/Matcher";
+import { TypedDynamicWhen } from "./TypedDynamicWhen";
 
-export interface TypedDynamicWhenOrElse<S, T> {
-  is: <U extends S>(
-    that: U,
-    returns: ((matched: U) => T) | T,
-  ) => TypedDynamicWhenOrElse<S, T>;
-
-  match: <U extends S>(
-    that: Matcher<S, U>,
-    returns: ((matched: U) => T) | T,
-  ) => TypedDynamicWhenOrElse<S, T>;
-
-  true: (
-    assertion: (() => boolean) | boolean,
-    returns: (() => T) | T,
-  ) => TypedDynamicWhenOrElse<S, T>;
-
-  else: (
-    returns: ((matched: S) => T) | T,
-  ) => T;
+export interface TypedDynamicWhenOrElse<S, T> extends TypedDynamicWhen<S, T> {
+  else: (returns: ((matched: S) => T) | T) => T;
 }
