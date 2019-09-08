02ca2d32959ea74d790d5655ad40222a77ac0405
nawordar <czarek@drozak.net>
2019-09-08 20:14:53 +0200

    Remove unnecessary import and add doc comment

--- src/types/When.ts
+++ src/types/When.ts
@@ -1,5 +1,4 @@
 import { DynamicWhen } from "./DynamicWhen";
-import { StaticWhen } from "./StaticWhen";
 import { StaticTrue, StaticNotNull } from "./StaticMethods";
 
 export interface When {
@@ -7,8 +6,9 @@ export interface When {
   /** Tests an object against multiple expressions and returns the result. */
   <T>(expr: T): DynamicWhen<T, never>;
 
-  /** Tests assertion and returns _value_ if assertion is true. */
+  /** Tests assertion and returns value if assertion is true. */
   true: StaticTrue<never>;
 
+  /** Returns value if it is not null */
   notNull: StaticNotNull<never>;
 }
