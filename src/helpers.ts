f15d96d5537d303380d010e5af73c94bbc82bcdf
cdrozak <c.drozak@nuadu.com>
2019-09-23 16:07:37 +0200

    Remove unnecessary function

--- src/helpers.ts
+++ src/helpers.ts
@@ -4,6 +4,3 @@ export const callOrReturn = <T>(subject: ((x?: any) => T) | T, value?: any): T =
     isCallable(subject)
         ? subject(value)
         : subject;
-
-export const isNotNull = (subject: ((x?: any) => any) | any, value?: any): boolean =>
-    callOrReturn(subject) != null;
